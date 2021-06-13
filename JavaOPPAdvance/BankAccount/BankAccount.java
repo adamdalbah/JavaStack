12eaa8b8006142cce9571ed106b5609b38922758
Adam Dalbah <adamdalbah@gmail.com>
2021-06-13 15:49:24 +0300

    Day3 Bank Account

---
+++ JavaOPPAdvance/BankAccount/BankAccount.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+public class BankAccount{
+    private String accountNumber;
+    private double checking_balance;
+    private double saving_balance;
+    private static int number_of_accounts = 0;
+    private static double total_money = 0;
+	
+    public BankAccount(){
+	this.accountNumber();
+	number_of_accounts ++;
+    }
+    private String accountNumber(){
+	Random r = new Random();
+	accountNumber = "";
+	for(int i = 0; i < 10; i++){
+	    accountNumber += r.nextInt(10);
+	}
+	return accountNumber;
+	
+    } 
+
+    public double getChecking_balance(){
+	return checking_balance;
+    }
+
+    public double getSaving_balance(){
+	return checking_balance;
+    }
+    public String getAccountNumber(){
+	return accountNumber;
+    }
+  
+    public void depositChecking(double money){
+	checking_balance += money;
+	total_money += money;
+    }
+
+    public void depositSaving(double money){
+	saving_balance += money;
+	total_money += money;
+    }
+    
+    public void withdrawSaving(double money){
+	if (money > saving_balance){
+	    System.out.println("You don't have suffiecent money");
+	}
+	else{
+	    saving_balance -= money;
+	    total_money -= money;
+	}	
+    }
+
+    public void showTotalMoney(){
+	System.out.println(this.total_money);
+    }
+
+}
---
+++ JavaOPPAdvance/BankAccount/BankAccountTest.java
@@ -0,0 +1,10 @@
+public class BankAccountTest{
+    public static void main(String[] args){
+	BankAccount khalil = new BankAccount();
+	khalil.depositSaving(200);
+	khalil.withdrawSaving(100);
+	khalil.showTotalMoney();
+	
+    }
+
+}
